4815201c8ffa31289156beab729208f20eb06e89
Yeongil Jung <yeongil.jung@devsisters.com>
2023-07-14 02:15:08 +0900

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# utterances 🔮
+
+This is a fork of [utterances](https://utteranc.es/) that I have slightly modified to self-host. One key advantage of self-hosting it is that I can use it on private GitHub repositories without adding a 3rd party bot as a collaborator to my repository.
---
+++ src/page-attributes.ts
@@ -0,0 +1,62 @@
+import { deparam } from './deparam';
+import repoRegex from './repo-regex';
+
+function readPageAttributes() {
+  const params = deparam(location.search.substr(1));
+
+  let issueTerm: string | null = null;
+  let issueNumber: number | null = null;
+  if ('issue-term' in params) {
+    issueTerm = params['issue-term'];
+    if (issueTerm !== undefined) {
+      if (issueTerm === '') {
+        throw new Error('When issue-term is specified, it cannot be blank.');
+      }
+      if (['title', 'url', 'pathname', 'og:title'].indexOf(issueTerm) !== -1) {
+        if (!params[issueTerm]) {
+          throw new Error(`Unable to find "${issueTerm}" metadata.`);
+        }
+        issueTerm = params[issueTerm];
+      }
+    }
+  } else if ('issue-number' in params) {
+    issueNumber = +params['issue-number'];
+    if (issueNumber.toString(10) !== params['issue-number']) {
+      throw new Error(`issue-number is invalid. "${params['issue-number']}`);
+    }
+  } else {
+    throw new Error('"issue-term" or "issue-number" must be specified.');
+  }
+
+  if (!('repo' in params)) {
+    throw new Error('"repo" is required.');
+  }
+
+  if (!('origin' in params)) {
+    throw new Error('"origin" is required.');
+  }
+
+  const matches = repoRegex.exec(params.repo);
+  if (matches === null) {
+    throw new Error(`Invalid repo: "${params.repo}"`);
+  }
+
+  // surge.sh's cleaning of URLs does not properly handle query params
+  const url = params.url.substr(-1, 0) === `/` ? params.url : `${params.url}/`
+
+  return {
+    owner: matches[1],
+    repo: matches[2],
+    issueTerm,
+    issueNumber,
+    origin: params.origin,
+    url: url,
+    title: params.title,
+    description: params.description,
+    label: params.label,
+    theme: params.theme || 'github-light',
+    session: params.session
+  };
+}
+
+export const pageAttributes = readPageAttributes();
